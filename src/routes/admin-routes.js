fc0439796e5c1da3db2068b812055d428a9ecf41
Anggri <aanganggriawan95@gmail.com>
2024-11-16 06:12:55 +0700

    push

--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -6,28 +6,60 @@ const sidebarMenu = [
   { name: "Dashboard", url: "/admin", icon: "bi bi-grid-fill", active: false },
 ];
 
-const subMenu = [
-  {
-    name: "Manage Theme",
-    url: "/admin/theme",
-    icon: "bi bi-file-check",
-    active: false,
-  },
-  {
-    name: "Manage Criteria",
-    url: "/admin/criteria",
-    icon: "bi bi-file-check",
-    active: false,
-  },
-  {
-    name: "Manage Indicator",
-    url: "/admin/indikator",
-    icon: "bi bi-file-check",
-    active: false,
-  },
-];
+const subMenu = {
+  assesment: [
+    {
+      name: "Manage Theme",
+      url: "/admin/theme",
+      icon: "bi bi-file-check",
+      active: false,
+    },
+    {
+      name: "Manage Criteria",
+      url: "/admin/criteria",
+      icon: "bi bi-file-check",
+      active: false,
+    },
+    {
+      name: "Manage Indicator",
+      url: "/admin/indikator",
+      icon: "bi bi-file-check",
+      active: false,
+    },
+  ],
+  landingPage: [
+    {
+      name: "Manage Event",
+      url: "admin/cms-event",
+      icon: "bi bi-file-check",
+      active: false,
+    },
+    {
+      name: "Manage News",
+      url: "/admin/criteria",
+      icon: "bi bi-file-check",
+      active: false,
+    },
+    {
+      name: "Manage Indicator",
+      url: "/admin/indikator",
+      icon: "bi bi-file-check",
+      active: false,
+    },
+  ],
+};
 
 const Criteria = [
+  {
+    name: "AM.1",
+    desc: "Reinvest profits from ecotourism development to maintain the quality of natural and cultural resources",
+    level: "Mandatory",
+    activities: false,
+    fasilities: true,
+    services: true,
+  },
+];
+const theme = [
   {
     name: "AM.1",
     desc: "Reinvest profits from ecotourism development to maintain the quality of natural and cultural resources",
@@ -42,7 +74,8 @@ router.get("/admin", (req, res) => {
   res.render("dashboard-admin", {
     content: "dashboard",
     sideMenu: sidebarMenu,
-    subMenu: subMenu,
+    subMenu: subMenu.assesment,
+    landingPage: subMenu.landingPage,
   });
 });
 
@@ -54,7 +87,8 @@ router.get("/admin/theme", (req, res) => {
     pages: [""],
     path_include: "contents/admin/theme",
     sideMenu: sidebarMenu,
-    subMenu: subMenu,
+    subMenu: subMenu.assesment,
+    landingPage: subMenu.landingPage,
     theme: theme,
   });
 });
@@ -67,7 +101,23 @@ router.get("/admin/criteria", (req, res) => {
     pages: [""],
     path_include: "contents/admin/criteria",
     sideMenu: sidebarMenu,
-    subMenu: subMenu,
+    subMenu: subMenu.assesment,
+    landingPage: subMenu.landingPage,
+    criteria: Criteria,
+  });
+});
+
+router.get("/admin/cms-event", (req, res) => {
+  res.render("dashboard-admin", {
+    content: "List Criteria",
+    title: "List Criteria",
+    subtitle: "Theme Criteria",
+    pages: [""],
+    path_include: "contents/admin/cms-event",
+    sideMenu: sidebarMenu,
+    subMenu: subMenu.assesment,
+    landingPage: subMenu.landingPage,
+
     criteria: Criteria,
   });
 });
